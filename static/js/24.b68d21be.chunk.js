02d632ba1659bdd03cdc381a941892bff3de672e
Gustavo Lee <gglee89@gmail.com>
2020-10-13 18:59:55 +0900

    Updates

---
+++ static/js/24.b68d21be.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@digimarket/digimarket"]=this["webpackJsonp@digimarket/digimarket"]||[]).push([[24],{642:function(i,t,a){},643:function(i,t,a){}}]);
+//# sourceMappingURL=24.b68d21be.chunk.js.map
